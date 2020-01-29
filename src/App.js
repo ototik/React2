071106f0e64150fdea1b58c6c735f8778e952a19
Orsolya Totik <ototik@gmail.com>
2020-01-29 17:15:59 +0100

    wed night commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+
+
+  function Opp () {
+       return (
+      <div className="Contact">
+         
+          <Contact
+          nameavatar="https://randomuser.me/api/portraits/men/22.jpg"
+          names="Joe Black"
+          status=" online"
+          />
+
+        <Contact
+          nameavatar="https://randomuser.me/api/portraits/women/44.jpg"
+          names="Nia Harris"
+          status=" online"
+          />    
+
+        <Contact
+          nameavatar="https://randomuser.me/api/portraits/women/6.jpg"
+          names="Clueless Lady"
+          status=" online"
+          />    
+        </div>
+           );
+    }
+   
+
+export default Opp;
