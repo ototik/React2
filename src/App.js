f2b687997d7ca2ba013cb6eab34958b45b407199
Orsolya Totik <ototik@gmail.com>
2020-01-30 10:24:32 +0100

    final commit thu morning

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React from 'react';
-import Contact from './components/Contact'
+import React from "react";
+import Contact from "./components/Contact";
 
+function Opp() {
+  return (
+    <div className="Contact">
+      <Contact
+        nameavatar="https://randomuser.me/api/portraits/men/22.jpg"
+        names="Joe Black"
+        text="offline"
+        status="false"
+      />
 
+      <Contact
+        nameavatar="https://randomuser.me/api/portraits/women/44.jpg"
+        names="Nia Harris"
+        text="online"
+        status="true"
+      />
 
-
-
-  function Opp () {
-       return (
-      <div className="Contact">
-         
-          <Contact
-          nameavatar="https://randomuser.me/api/portraits/men/22.jpg"
-          names="Joe Black"
-          status="online"
-          />
-
-        <Contact
-          nameavatar="https://randomuser.me/api/portraits/women/44.jpg"
-          names="Nia Harris"
-          status="online"
-          />    
-
-        <Contact
-          nameavatar="https://randomuser.me/api/portraits/women/6.jpg"
-          names="Clueless Lady"
-          status="online"
-          />    
-        </div>
-           );
-    }
-   
+      <Contact
+        nameavatar="https://randomuser.me/api/portraits/women/6.jpg"
+        names="Clueless Lady"
+        text="online"
+        status="true"
+      />
+    </div>
+  );
+}
 
 export default Opp;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import './Contactcss.css';
-
-
+import React from "react";
+import "./Contactcss.css";
 
 function Contact(props) {
-    return (
+  return (
     <div className="Contact">
-      
-      <img className="avatar" src= {props.nameavatar} alt={props.names} />
-      <div className="name" >{props.names} </div>
+      <img className="avatar" src={props.nameavatar} alt={props.names} />
+      <div className="name">{props.names} </div>
 
-   
 
       <div className="status">
-
-        <div 
-          className="status-text">{
-        props.text === "online" ? "online" : "offline"
-      }</div>
-
-     
+        <div className="format">
+        <div className="status-text">
+          {props.text === "online" ? "online" : "offline"}
+        </div>
+        {props.status === "true" ? (
+          <div className="status-online" />
+        ) : (
+          <div className="status-offline" />
+        )}
       </div>
       </div>
-    );
-    }
+    </div>
+  );
+}
 
 export default Contact;
